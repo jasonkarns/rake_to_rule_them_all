7706fa9df74b3cf1c8e0faea7dfd50c05fe9e24b
Jason Karns <jason@karns.name>
2018-01-15 21:17:36 -0500

    classify

--- lib/rake_to_rule_them_all/tasks.rb
+++ lib/rake_to_rule_them_all/tasks.rb
@@ -1,14 +1,41 @@
+require 'rake'
+require 'rake/tasklib'
+
+module RakeToRuleThemAll
+  class RakeTask < ::Rake::TaskLib
+    include ::Rake::DSL if defined?(::Rake::DSL)
+
+    attr_reader :script
+
+    def initialize(script)
+      @script = script
+      define
+    end
+
+    def name
+      File.basename script
+    end
+
+    def description
+      File.new(script).each_line
+        .take_while { |line| line =~ /^(#|$)/ }
+        .map { |line| line[/^# (.*)$/, 1] }
+        .join("\n")
+    end
+
+    private
+
+    def define
+      desc description
+      task name do
+        sh script
+      end
+    end
+
+  end
+end
 
 SCRIPTS = FileList['script/*']
 SCRIPTS.each do |script|
-  name = script.pathmap('%n')
-
-  desc File.new(script).each_line
-    .take_while { |line| line =~ /^(#|$)/ }
-    .map { |line| line[/^# (.*)$/, 1] }
-    .join("\n")
-
-  task name do
-    sh script
-  end
+  RakeToRuleThemAll::RakeTask.new script
 end
